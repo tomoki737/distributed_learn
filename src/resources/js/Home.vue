cd33115e7f421bb3f2a99d45e7241c5864624bd2
tomoki737 <tomoki.k2159@gmail.com>
2022-04-09 18:46:31 +0900

    ローディングの追加

--- src/resources/js/Home.vue
+++ src/resources/js/Home.vue
@@ -1,32 +1,55 @@
 <template>
-  <v-card elevation="2" class="mt-10 mx-auto" width="600px">
-    <v-row>
-      <v-col cols="6">
-        <v-card-text>
-          <div class="red text-center">
-            <p>新しく覚える</p>
-            <h3>0</h3>
-          </div>
-        </v-card-text>
-      </v-col>
-      <v-col cols="6">
-        <v-card-text>
-          <div class="blue text-center">
-            <p>復習</p>
-            <h3>0</h3>
-          </div>
-        </v-card-text>
-      </v-col>
-    </v-row>
-    <v-card-actions>
-      <div class="mx-auto">
-      <v-btn router-link :to="{ name: 'question.answer' }"> 学習する </v-btn>
-      </div>
-    </v-card-actions>
-  </v-card>
+  <div>
+    <loading :loading="loading"></loading>
+    <v-card elevation="2" class="mt-10 mx-auto" width="600px" v-show="!loading">
+      <v-row>
+        <v-col cols="6">
+          <v-card-text>
+            <div class="red text-center">
+              <p>新しく覚える</p>
+              <h3>0</h3>
+            </div>
+          </v-card-text>
+        </v-col>
+        <v-col cols="6">
+          <v-card-text>
+            <div class="blue text-center">
+              <p>復習</p>
+              <h3>{{ review }}</h3>
+            </div>
+          </v-card-text>
+        </v-col>
+      </v-row>
+      <v-card-actions>
+        <div class="mx-auto">
+          <v-btn router-link :to="{ name: 'question.answer' }">
+            学習する
+          </v-btn>
+        </div>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 <script>
+import Loading from "./components/Loading.vue";
 export default {
-  components: {},
+  components: { Loading },
+  data() {
+    return {
+      review: "0",
+      loading: true,
+    };
+  },
+  methods: {
+    getQuestions() {
+      axios.get("/api/answer").then((response) => {
+        this.review = response.data.questions.length;
+        this.loading = false;
+      });
+    },
+  },
+  mounted() {
+    this.getQuestions();
+  },
 };
 </script>
--- src/resources/js/question/QuestionAnswer.vue
+++ src/resources/js/question/QuestionAnswer.vue
@@ -1,33 +1,38 @@
 <template>
-  <v-container>
-    <v-card elevation="2" class="mt-10 mx-auto" width="600px" height="500px">
-      <v-toolbar color="blue lighten-3" class="white--text" flat>
-        <v-btn
-          icon
-          color="white"
-          @click="question_change(show_question ? false : true)"
-          v-show="show_answer"
-          ><v-icon>mdi-sync</v-icon></v-btn
-        >
-        <h3 class="mx-auto">{{ show_question ? "問題" : "解答" }}</h3>
-      </v-toolbar>
-      <v-card-text class="text-center mt-3">
-        <h3 v-show="show_question">{{ questions[number].question }}</h3>
-        <h3 v-show="!show_question">{{ questions[number].answer }}</h3>
-      </v-card-text>
-    </v-card>
-    <div class="text-center mt-7" v-if="!show_answer">
-      <v-btn color="primary" @click="answer_change"> 答えを見る </v-btn>
-    </div>
-    <div class="text-center mt-7" v-else>
-      <v-btn @click="next(questions[number], true)">わかった</v-btn>
-      <v-btn @click="next(questions[number], false)">わからない</v-btn>
-    </div>
-  </v-container>
+  <div>
+    <loading :loading="loading"></loading>
+    <v-container v-show="!loading">
+      <v-card elevation="2" class="mt-10 mx-auto" width="600px" height="500px">
+        <v-toolbar color="blue lighten-3" class="white--text" flat>
+          <v-btn
+            icon
+            color="white"
+            @click="question_change(show_question ? false : true)"
+            v-show="show_answer"
+            ><v-icon>mdi-sync</v-icon></v-btn
+          >
+          <h3 class="mx-auto">{{ show_question ? "問題" : "解答" }}</h3>
+        </v-toolbar>
+        <v-card-text class="text-center mt-3">
+          <h3 v-show="show_question">{{ questions[number].question }}</h3>
+          <h3 v-show="!show_question">{{ questions[number].answer }}</h3>
+        </v-card-text>
+      </v-card>
+      <div class="text-center mt-7" v-if="!show_answer">
+        <v-btn color="primary" @click="answer_change"> 答えを見る </v-btn>
+      </div>
+      <div class="text-center mt-7" v-else>
+        <v-btn @click="next(questions[number], true)">わかった</v-btn>
+        <v-btn @click="next(questions[number], false)">わからない</v-btn>
+      </div>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import Loading from "../components/Loading.vue";
 export default {
+  components: { Loading },
   data() {
     return {
       number: 0,

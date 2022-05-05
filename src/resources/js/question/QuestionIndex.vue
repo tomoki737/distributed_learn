a02dfbeb511237ebbf6a48f48f4e343c4a312a5f
tomoki737 <tomoki.k2159@gmail.com>
2022-05-05 15:25:05 +0900

    非同期処理をasyncに変更 #36

--- src/resources/js/question/QuestionIndex.vue
+++ src/resources/js/question/QuestionIndex.vue
@@ -1,58 +1,53 @@
 <template>
   <div>
     <loading :loading="loading"></loading>
-    <v-container v-show="!loading" style="max-width:1000px" class="mt-2">
-     <template>
-  <div class="text-end">
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="blue"
-          dark
-          v-bind="attrs"
-          v-on="on"
-          outlined
-        >
-          <v-icon>mdi-magnify</v-icon>
-        </v-btn>
-      </template>
+    <v-container v-show="!loading" style="max-width: 1000px" class="mt-2">
+      <div class="text-end">
+        <v-dialog v-model="dialog" width="500">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="blue" dark v-bind="attrs" v-on="on" outlined>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+          </template>
 
-      <v-card>
-        <v-card-title class="text-h5 blue lighten-3">
-          検索条件
-        </v-card-title>
-        <v-divider></v-divider>
+          <v-card>
+            <v-card-title class="text-h5 blue lighten-3">
+              検索条件
+            </v-card-title>
+            <v-divider></v-divider>
 
-      <v-card-text>
-        <question-tags-input :placeholder="'タグ'" :autocompleteItems="allTagNames" class="mt-3"></question-tags-input>
-        <v-text-field label="キーワード" v-model="searchForm.keyword"></v-text-field>
-      </v-card-text>
+            <v-card-text>
+              <question-tags-input
+                :placeholder="'タグ'"
+                :autocompleteItems="allTagNames"
+                class="mt-3"
+              ></question-tags-input>
+              <v-text-field
+                label="キーワード"
+                v-model="searchForm.keyword"
+              ></v-text-field>
+            </v-card-text>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
+            <v-card-actions>
+              <v-spacer></v-spacer>
 
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
-          >
-            キャンセル
-          </v-btn>
-          <v-btn
-            color="primary"
-            text
-            @click="search(); dialog = false"
-          >
-            検索
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
-</template>
+              <v-btn color="primary" text @click="dialog = false">
+                キャンセル
+              </v-btn>
+              <v-btn
+                color="primary"
+                text
+                @click="
+                  search();
+                  dialog = false;
+                "
+              >
+                検索
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
       <div v-for="question in questions" :key="question.id">
         <question-index-card
           :question="question"
@@ -63,10 +58,11 @@
   </div>
 </template>
 
+
 <script>
 import Loading from "../components/Loading.vue";
 import QuestionIndexCard from "../components/QuestionIndexCard.vue";
-import QuestionTagsInput from "../components/QuestionTagsInput.vue"
+import QuestionTagsInput from "../components/QuestionTagsInput.vue";
 export default {
   components: {
     Loading,
@@ -78,30 +74,30 @@ export default {
     return {
       questions: [],
       loading: true,
-      dialog:false,
+      dialog: false,
       allTagNames: [],
       searchForm: {
-          tag:[],
-          keyword: "",
-      }
+        tag: [],
+        keyword: "",
+      },
     };
   },
 
   methods: {
-    getQuestions() {
-      axios.get("/api/question").then((res) => {
-        this.questions = res.data.questions;
-        this.allTagNames = res.data.allTagNames;
-        this.loading = false;
-      });
+    async getQuestions() {
+      const response = await axios.get("/api/question");
+      this.questions = response.data.questions;
+      this.allTagNames = response.data.allTagNames;
+      this.loading = false;
+      console.log(this.questions);
     },
-    search() {
-        this.loading = true;
-        axios.post("/api/question/search", this.searchForm).then(res => {
-            this.questions = res.data.questions;
-        })
-        this.loading = false;
-    }
+    async search() {
+      const response = await axios.post(
+        "/api/question/search",
+        this.searchForm
+      );
+      this.questions = response.data.questions;
+    },
   },
 
   mounted() {

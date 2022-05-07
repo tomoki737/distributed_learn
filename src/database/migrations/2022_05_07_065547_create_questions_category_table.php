b280adc4d799b3c72ea119fdbe36da5013a6c36f
tomoki737 <tomoki.k2159@gmail.com>
2022-05-07 16:00:54 +0900

    カテゴリー関連テーブルの作成 40

--- src/database/migrations/2022_05_07_065547_create_questions_category_table.php
+++ src/database/migrations/2022_05_07_065547_create_questions_category_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuestionTagTable extends Migration
+class CreateQuestionsCategoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,17 @@ class CreateQuestionTagTable extends Migration
      */
     public function up()
     {
-        Schema::create('question_tag', function (Blueprint $table) {
+        Schema::create('category', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('question_id')->unsigned();
+            $table->bigInteger('question_id')->unsigned();;
             $table->foreign('question_id')
                 ->references('id')
                 ->on('questions')
                 ->onDelete('cascade');
-            $table->bigInteger('tag_id')->unsigned();
-            $table->foreign('tag_id')
+            $table->bigInteger('category_id')->unsigned();;
+            $table->foreign('category_id')
                 ->references('id')
-                ->on('tags')
+                ->on('categories')
                 ->onDelete('cascade');
             $table->timestamps();
         });
@@ -36,6 +36,6 @@ class CreateQuestionTagTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('question_tag');
+        Schema::dropIfExists('category');
     }
 }

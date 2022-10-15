fd4f5ac0fee96d230fa159c5dd844e20cba541ad
tomoki737 <tomoki.k2159@gmail.com>
2022-10-15 23:47:24 +0900

    コンパイル 変更 修正

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -10,12 +10,12 @@
     </title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-    <link rel="stylesheet" href="{{ assets('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
     <div id="app"></div>
-    <script src="{{ assets('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>

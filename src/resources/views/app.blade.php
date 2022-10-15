0da07d7054029813673d47b7471d20c9caa1922e
tomoki737 <tomoki.k2159@gmail.com>
2022-10-15 22:05:04 +0900

    stylesheetをassetコンパイルに変更 修正

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -10,12 +10,12 @@
     </title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type='application/json'>
+    <link rel="stylesheet" href="{{ mix('css/app.css', 'css') }}">
 </head>
 
 <body>
     <div id="app"></div>
-    <script src="{{ asset('js/app.js') }}" type='application/json'></script>
+    <script src="{{ mix('js/app.js', 'js') }}"></script>
 </body>
 
 </html>

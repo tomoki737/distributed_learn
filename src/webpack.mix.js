0ac9d30e4d4e18f41bb108d00db21d8acef83afd
tomoki737 <tomoki.k2159@gmail.com>
2022-10-16 08:40:29 +0900

    コンパイル 変更 修正

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
 
  mix.js('resources/js/app.js', 'public/js').vue()
- .postCss('resources/css/app.css', 'public/css')
- .version();
+ .postCss('resources/css/app.css', 'public/css');

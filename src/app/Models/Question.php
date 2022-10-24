3d0bc81c935088e3aacfece0e68ff12c9585a639
tomoki737 <tomoki.k2159@gmail.com>
2022-10-24 20:53:00 +0900

    いいねテーブルの作成 #156

--- src/app/Models/Question.php
+++ src/app/Models/Question.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use FunctionName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -44,5 +45,17 @@ class Question extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\Models\User');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->BelongsToMany('App\Models\User', 'likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
     }
 }
